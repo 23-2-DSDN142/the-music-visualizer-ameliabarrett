538ee10a2acb3dd6c362df95df6606c14cd71dca
ameliabarrett <ameliarbarrett@icloud.com>
2023-10-10 23:27:31 +1300

    Update music_starter.js

--- music_starter.js
+++ music_starter.js
@@ -11,31 +11,32 @@ function draw_one_frame(words, vocal, drum, bass, other, counter) {
 
 //DRUMS
 //stars
-var drumMap = map(drum, 0, 100, 5, 20);
+var drumMap = map(drum, 0, 100, 0, 5 );
   
-for (var ii = 1; ii <= 10; ii++) {
-  var yStar = ii * 50;
-  for (var i = 1; i <= 10; i++) {
+for (var ii = 1; ii <= 50; ii++) { // how many across x
+  var yStar = ii * 20;
+  for (var i = 1; i <= 30; i++) { //how many y 
     fill(193, 177, 231); 
-    ellipse(100 * i, yStar, drumMap);
+    let r = random(50);
+    ellipse(50 * i + r, yStar , drumMap);
   }
 }
-
 
   // BASS
   //background circle with lines cutting
   let circleRadius = 250;
-  let numLines = 12;
-  let lineSpacing = 60;
   
   fill(193, 177, 231); // Pale whitey purple
   ellipse(width / 2, height / 2, circleRadius * 2, circleRadius * 2);
   
-  for (let i = 0; i < numLines; i++) {
-    let y = height / 2 - circleRadius + i * lineSpacing;
-    strokeWeight(20);
-    stroke(5, 1, 36); //navy blue
-    line(width / 2 - circleRadius, y, width / 2 + circleRadius, y);
+
+  let stripeWidth = map(other, 40, 100, 40, 70, true);
+
+  let numStripes = height / stripeWidth;
+  for(let i=0; i<numStripes; i=i+2) {
+    let cury = map(i, 0, numStripes-1, 0, height);
+    fill(5, 1, 36); //navy blue
+      rect(0, cury, width, stripeWidth);
   }
     
     
