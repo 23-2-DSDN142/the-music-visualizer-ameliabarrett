80cf277f24992aa9a389ca52d8a5b686caea047a
ameliabarrett <ameliarbarrett@icloud.com>
2023-10-11 18:31:30 +1300

    Update music_starter.js

--- music_starter.js
+++ music_starter.js
@@ -9,19 +9,6 @@ function draw_one_frame(words, vocal, drum, bass, other, counter) {
   // BACKGROUND
    background(5, 1, 36); //navy blue
 
-//DRUMS
-//stars
-var drumMap = map(drum, 0, 100, 0, 5 );
-  
-for (var ii = 1; ii <= 50; ii++) { // how many across x
-  var yStar = ii * 20;
-  for (var i = 1; i <= 30; i++) { //how many y 
-    fill(193, 177, 231); 
-    let r = random(50);
-    ellipse(50 * i + r, yStar , drumMap);
-  }
-}
-
   // BASS
   //background circle with lines cutting
   let circleRadius = 250;
@@ -36,9 +23,22 @@ for (var ii = 1; ii <= 50; ii++) { // how many across x
   for(let i=0; i<numStripes; i=i+2) {
     let cury = map(i, 0, numStripes-1, 0, height);
     fill(5, 1, 36); //navy blue
-      rect(0, cury, width, stripeWidth);
+      rect(0, cury, width, other);
   }
     
+  //DRUMS
+//stars
+var drumMap = map(drum, 0, 100, 0, 5 );
+  
+for (var ii = 1; ii <= 50; ii++) { // how many across x
+  var yStar = ii * 5;
+  for (var i = 1; i <= 30; i++) { //how many y 
+    noStroke();
+    fill(193, 177, 231); 
+    let r = random(50);
+    ellipse(50 * i + r, yStar , drumMap);
+  }
+}
     
  }
 
